a7a2d980b5e1ca3f4c6c918c271fde107a22e8c6
Tommy Balestreri <tcbstl@gmail.com>
2020-02-01 11:30:08 -0600

    Assignment 4 - Starting to work on PART 4 - MANY TO MANY RELATIONSHIP - About to update home controller

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -2,6 +2,8 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
@@ -10,12 +12,16 @@ public class Job extends AbstractEntity {
     @ManyToOne
     public Employer employer;
 
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+
+//    private String skills;
 
     public Job() {
     }
 
-    public Job(Employer anEmployer, String someSkills) {
+//    public Job(Employer anEmployer, String someSkills) {
+public Job(Employer anEmployer, List<Skill> someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -29,11 +35,18 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
+//    public String getSkills() {
+//        return skills;
+//    }
+//
+//    public void setSkills(String skills) {
+//        this.skills = skills;
+//    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,7 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -9,11 +13,23 @@ public class Skill extends AbstractEntity {
     @Size(max = 250)
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+
     public Skill() {
     }
 
     public String getDescription() {
         return description;
+    }
+
+//    public List<Skill> getSkills() {
+//        return skills;
+//    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
     public void setDescription(String description) {
